0ea551c9a07ae494f5e311d4c9c0a95e6d9fe6e0
Mohammadreza <bakhtiarimohammadreza2@gmail.com>
2022-03-16 13:42:11 +0330

    Add project files.

---
+++ UniversityProject.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityProject.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
