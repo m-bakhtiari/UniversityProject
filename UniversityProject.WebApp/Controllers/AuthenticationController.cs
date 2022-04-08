373d6eafb1aa3155ae499f3cffb87a8f939a3786
Mohammadreza <bakhtiarimohammadreza2@gmail.com>
2022-04-09 01:13:39 +0430

    Fix book box button

---
+++ UniversityProject.WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UniversityProject.Core.DTOs;
+
+namespace UniversityProject.WebApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            return null;
+        }
+    }
+}
